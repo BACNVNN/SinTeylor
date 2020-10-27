645dc90fac882397ca0f6a542236e8e4b63cab0e
Василий Окишев <vasyaokishev@gmail.com>
2020-10-27 06:23:16 +0300

    Без погрешностей

--- SinTeylor/Program.cs
+++ SinTeylor/Program.cs
@@ -8,33 +8,31 @@ namespace SinTeylor
         {
             Console.WriteLine("Для того чтобы программа могла рассчитать sin(x), введите x (в градусах)");
             double a = Convert.ToDouble(Console.ReadLine());
-            Series(a);
-           
+
+            Console.WriteLine( "sin({0}) = {1}", a, Series(a) );
+
             Console.ReadLine(); // для того чтобы окно не закрывалось сразу после вывода результата
             
         }
-        static void Series(double x) // Метод, задающий ряд Тейлора для синуса
+        static double Series(double x) // Метод, задающий и вычисляющий ряд Тейлора для синуса
         {
-            double s = 0; // создание суммы ряда
+            double Taylor = 0; // создание суммы ряда
             int k = 1;
             double j = 1;
-            for (int i = 0; i <= 250; i++) // разложение sin(x) в ряд Тейлора {x - x^3 / 3! + x^5 / 5! + .... -x^249 / 249!}
+            for (int i = 0; i <= 201; i++) // разложение sin(x) в ряд Тейлора {x - x^3 / 3! + x^5 / 5! + .... -x^201 / 201!}
             {
                if (i % 2 != 0)
                 {
-                    double a = k * Math.Pow(x*Math.PI/180, i); // числитель ощего члена ряда
-                    s += a / j;
+                    double TaylorI = k * Math.Pow(x*Math.PI/180, i); // числитель ощего члена ряда
+                    Taylor += TaylorI / j;
                     j *= (i+1)*(i+2); // факториал в знаментале
                     k *= -1; // ряд знакочередующийся
                 }
-                s = Math.Round(s, 7); // округление ответа до 9ого знака после запятой
+                Taylor = Math.Round(Taylor, 9); // округление ответа
             }
-            Console.WriteLine("sin({0}) = {1}", x, s);
+            return Taylor;
 
         }
-        static void Err(double abserr, double err, double value)
-        {
-            double err = Math.Sin(x*Math.PI/180) - 
-        }
+       
     }
 }
